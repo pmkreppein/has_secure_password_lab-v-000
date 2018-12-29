25e95ac26420199166f7583526336078c9f275c3
Peter Kreppein <pmkreppein@gmail.com>
2018-12-29 01:17:57 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,23 @@
 class SessionsController < ApplicationController
+
   def new
   end
-  
-  def create 
-    if params[:user][:password] == nil || params[:user][:password].empty?
-      
-  end 
+
+  def create
+    if  params[:user][:password] == nil || params[:user][:password].empty?
+      flash[:notice] = "Please fill in all information to login."
+      redirect_to controller: 'sessions', action: 'new'
+    else
+      @user = User.find_by(name: params[:user][:name])
+      @user = @user.try(:authenticate, params[:user][:password])
+      if @user
+        session[:user_id] = @user.id
+        redirect_to controller: 'users', action: 'index'
+      else
+        flash[:notice] = "The password for this user is incorrect."
+        redirect_to controller: 'sessions', action: 'new'
+      end
+    end
+  end
 
 end
