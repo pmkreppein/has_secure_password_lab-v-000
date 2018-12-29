d85b463331e9ca1c2932a64f20ff3ae9ad95a087
Peter Kreppein <pmkreppein@gmail.com>
2018-12-29 00:27:45 +0000

    Automatically backed up by Learn

--- db/migrate/20181229002656_create_users.rb
+++ db/migrate/20181229002656_create_users.rb
@@ -1,8 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
-      t.timestamps null: false
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.timestamps null: false
     end
   end
 end
